0b474aa4b263b43cfedf0774be72034c8a7526f6
Sydney Bruce <sydneybruce20@gmail.com>
2023-11-28 17:31:58 -0700

    added all post content, still need markdown and datejs implimentation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,15 @@
 
 <!-- <h1>{data.a3_blog.title}</h1> -->
 {#each data.a3_blog as post}
-<li>{post.title}</li>
+<h1>{post.title}</h1>
+<p>{post.summary}</p>
+<p>{post.author}</p>
+
+  {#each post.tags as tag}
+  <li>{tag}</li>
+  {/each}
+<p>{post.markdown_content}</p>
+<p>{post.date}</p>
 {/each}
 
 
